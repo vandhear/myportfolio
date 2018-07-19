22e04b46daf8288110f5318f021f9d332605dadf
vandhear <wulf.sheleg@gmail.com>
2018-07-19 23:30:45 +0300

    First Commit

---
+++ koroth/js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("navig").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("navig").style.marginLeft= "0";
+}
